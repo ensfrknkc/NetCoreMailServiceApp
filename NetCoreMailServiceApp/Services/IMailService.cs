8ec526e6b5b62063e504cbac772bf06e897c79ef
ensfrknkc <ensfrknkc58@gmail.com>
2022-03-13 17:52:09 +0300

    MailConfigurationComplated

---
+++ NetCoreMailServiceApp/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using NetCoreMailServiceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMailServiceApp.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
--- NetCoreMailServiceApp/Startup.cs
+++ NetCoreMailServiceApp/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NetCoreMailServiceApp.Services;
+using NetCoreMailServiceApp.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,8 @@ namespace NetCoreMailServiceApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+            services.AddTransient<IMailService, Services.MailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
